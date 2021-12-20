1097b460651e1d2cf3e9aa6e63dbcb8c9c9a9d43
Eric Kilmer <eric.d.kilmer@gmail.com>
2021-12-20 09:54:42 -0500

    CMake Revamp
    Many changes to use more idiomatic CMake

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -20,6 +20,7 @@
 // License: MIT
 //=============================================================================
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
